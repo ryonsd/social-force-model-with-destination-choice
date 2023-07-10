80305b03c25829273615b290da4e5f08edee753b
m-june <miwa.2023@aist.go.jp>
2023-07-10 16:13:32 +0900

    0710,ラッパーで動かせるように

--- june/make_trajectory.py
+++ june/make_trajectory.py
@@ -4,8 +4,15 @@ import pandas as pd
 from statistics import mean, stdev
 from tqdm.notebook import trange, tqdm
 import warnings
+import argparse
+import torch
+import time
+from tqdm import tqdm
+import yaml
+import os
 warnings.simplefilter('ignore')
 
+from SFM import make_sfm
 import sys
 sys.path.append("../src")
 
@@ -13,5 +20,28 @@ from main import Agent, run
 # from make_sfm import run_sfm
 from social_force_model import SocialForceModel
 from destination_choice_model import DestinationChoiceModel
-from SFM import run_SFM
 
+#yamlから読み込むデータパスを指定
+#データパスを使ってmake_sfmを実行
+
+if __name__ == '__main__':
+    file_path = 'config.yaml'
+    with open(file_path) as f:
+        config = yaml.safe_load(f)
+    data_path = config['input']
+    save_path = config['output']
+    device = config['device']
+    for i in range(len(data_path)):
+        dp = data_path[i]
+        sp = save_path[i]
+        command = 'python SFM.py -i ' + dp + ' -o ' + sp + ' -d ' + device
+        print(command)
+        command = command.strip() + '\n'
+        run = os.system(command)
+        print('command number:', i)
+        if run == 0:
+            print('success')
+        else:
+            print('failed')
+    print('finish')
+    
